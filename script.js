5bae60bfca1723f2ba4215cee6558d400383d77e
alb0z32 <70151449+alb0z32@users.noreply.github.com>
2023-01-02 01:41:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,6 @@ const fetchData = async (url) => {
         const data = await response.json();
         console.log(data)
         return data;
-
     } catch (e) {
         console.log(e);
     }
